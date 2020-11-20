4582acdb749b3fb54f5a2582cab5f2f1ca41fd3e
UltraProton <cs18mtech11008@iith.ac.in>
2020-11-20 21:28:07 +0530

    Completed UI Change to Recycler View.

--- app/src/main/java/com/example/batterycheck/MainActivity.java
+++ app/src/main/java/com/example/batterycheck/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.batterycheck;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -32,10 +31,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "Boom Boom";
-
+    ArrayList<app_data> apps_list;
     MyAdapter myAdapter;
     RecyclerView recyclerView;
-    //private AppAdapter power_appAdapter;
     ListView power_apps_view;
     List<ActivityManager.RunningAppProcessInfo> processes;
     ActivityManager amg;
@@ -44,154 +42,113 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //power_apps_view = (ListView) findViewById(R.id.app_list);
-
-        myAdapter= new MyAdapter(this);
+        apps_list=get_apps_list();
+        myAdapter= new MyAdapter(this,apps_list);
         recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
 
         //attach the adapter to the recycler view
         recyclerView.setAdapter(myAdapter);
         //Set which layout you want to see in the recycler view
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //recyclerView.setLayoutManager(new GridLayoutManager(this,2));
 
-//        power_appAdapter = new AppAdapter(MainActivity.this, apps_list);
-//        power_apps_view.setAdapter(power_appAdapter);
-//        power_apps_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView,  View view, final int i, long l) {
-//                ActivityManager mActivityManager = (ActivityManager) MainActivity.this.getSystemService(Context.ACTIVITY_SERVICE);
-//                String[] colors = {" Open "+apps_list.get(i).getName(), " App Info", "Kill App"};
-//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-//                builder
-//                        .setItems(colors, new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                // The 'which' argument contains the index position of the selected item
-//                                if (which==0){
-//                                    Intent intent = getPackageManager().getLaunchIntentForPackage(apps_list.get(i).packages);
-//                                    if(intent != null){
-//                                        startActivity(intent);
-//                                    }
-//                                    else {
-//                                        Toast.makeText(MainActivity.this, apps_list.get(i).packages + " Error, Please Try Again...", Toast.LENGTH_SHORT).show();
-//                                    }
-//                                }
-//                                if (which==1){
-//                                    Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-//                                    intent.setData(Uri.parse("package:" + apps_list.get(i).packages));
-////                                    Toast.makeText(MainActivity.this, apps_list.get(i).getName() , Toast.LENGTH_SHORT).show();
-//                                    startActivity(intent);
-//                                }
-//                                if(which==2)
-//                                {
-//                                    mActivityManager.killBackgroundProcesses(apps_list.get(i).getPackages());
-//                                    Log.d(TAG, "onClick: "+apps_list.get(i).getPackages());
-//                                    Toast.makeText(getApplicationContext(), apps_list.get(i).getName()+" killed",Toast.LENGTH_LONG).show();
-//
-//                                }
-//                            }
-//                        });
-//                builder.show();
-//            }
-//        });
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(MainActivity.this, recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                TextView app_name = (TextView) findViewById(R.id.app_name);
+                Log.i("App name: ",""+apps_list.get(position).name);
 
-        //Total Number of Installed-Apps(i.e. List Size)
-//        String  abc = power_apps_view.getCount()+"";
-//        TextView countApps = (TextView)findViewById(R.id.countApps);
-//        countApps.setText("Total Installed Apps: "+abc);
-//        Toast.makeText(this, abc+" Apps", Toast.LENGTH_SHORT).show();
+                ActivityManager mActivityManager = (ActivityManager) MainActivity.this.getSystemService(Context.ACTIVITY_SERVICE);
+                String[] colors = {" Open "+apps_list.get(position).getName(), " App Info", "Kill App"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder
+                        .setItems(colors, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // The 'which' argument contains the index position of the selected item
+                                if (which==0){
+                                    Intent intent = getPackageManager().getLaunchIntentForPackage(apps_list.get(position).getPackages());
+                                    if(intent != null){
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        Toast.makeText(MainActivity.this, apps_list.get(position).getPackages() + " Error, Please Try Again...", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                if (which==1){
+                                    Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.setData(Uri.parse("package:" + apps_list.get(position).packages));
+//                                    Toast.makeText(MainActivity.this, apps_list.get(i).getName() , Toast.LENGTH_SHORT).show();
+                                    startActivity(intent);
+                                }
+                                if(which==2)
+                                {
+                                    mActivityManager.killBackgroundProcesses(apps_list.get(position).getPackages());
+                                    Log.d(TAG, "onClick: "+apps_list.get(position).getPackages());
+                                    Toast.makeText(getApplicationContext(), apps_list.get(position).getName()+" killed",Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+                builder.show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
 
     }
 
-    //Get all the apps apart from system apps
-//
-//    public class AppAdapter extends BaseAdapter {
-//
-//        public LayoutInflater layoutInflater;
-//        public List<app_data> listStorage;
-//
-//        public AppAdapter(Context context, List<app_data> customizedListView) {
-//            layoutInflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            listStorage = customizedListView;
-//
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return listStorage.size();
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return listStorage.get(position);
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return position;
-//        }
-//
-//        //We need to overload the getViewTypeCount() and getItemViewType() to remove the multiple
-//        //colors in multiple item in the list view
-//        @Override
-//        public int getViewTypeCount() {
-//            return getCount();
-//        }
-//
-//        @Override
-//        public int getItemViewType(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            //int mColor= 0xFF000000;
-//            ViewHolder listViewHolder;
-//            if(convertView == null){
-//                listViewHolder = new ViewHolder();
-//                convertView = layoutInflater.inflate(R.layout.app_list, parent, false);
-//                listViewHolder.textInListView = (TextView)convertView.findViewById(R.id.app_name);
-//                listViewHolder.imageInListView = (ImageView)convertView.findViewById(R.id.app_icon);
-//                listViewHolder.packageInListView=(TextView)convertView.findViewById(R.id.app_package_name);
-//                listViewHolder.layout= (LinearLayout) convertView.findViewById(R.id.layout);
-//                convertView.setTag(listViewHolder);
-//            }
-//            else{
-//                listViewHolder = (ViewHolder)convertView.getTag();
-//            }
-//
-//            listViewHolder.textInListView.setText(listStorage.get(position).getName());
-//            Log.d(TAG, "getView: "+ listStorage.get(position).getName());
-//            String x=listStorage.get(position).getName();
-//
-//            if(x.equals("Asynctask"))
-//            {
-//                //listViewHolder.layout.setBackgroundColor();
-//                listViewHolder.layout.setBackgroundColor(Color.parseColor("#EF5350"));
-//            }
-//            //Log.i("itemmm",""+getItem(position).toString());
-//            //if(getItem(position))
-//            listViewHolder.imageInListView.setImageDrawable(listStorage.get(position).getIcon());
-//            listViewHolder.packageInListView.setText(listStorage.get(position).getPackages());
-//
-//            return convertView;
-//        }
-//
-//        class ViewHolder{
-//            TextView textInListView;
-//            ImageView imageInListView;
-//            TextView packageInListView;
-//            LinearLayout layout;
-//        }
-//    }
 
+    public class app_data {
+        private String name;
+        Drawable icon;
+        private String packages;
 
-//        public String getA(){
-//            return a;
-//        }
-//        public int getD() {
-//            return d;
-//        }
+        public app_data(String name, Drawable icon, String packages) {   //}, String a, int d) {
+            this.name = name;
+            this.icon = icon;
+            this.packages = packages;
 
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Drawable getIcon() {
+            return icon;
+        }
+
+        public String getPackages() {
+            return packages;
+        }
+    }
+
+    private ArrayList<app_data> get_apps_list() {
+        PackageManager pm = this.getPackageManager();
+        ArrayList<app_data> apps = new ArrayList<app_data>();
+        List<PackageInfo> packs = pm.getInstalledPackages(0);
+        //List<PackageInfo> packs = getPackageManager().getInstalledPackages(PackageManager.GET_PERMISSIONS);
+        for (int i = 0; i < packs.size(); i++) {
+            PackageInfo p = packs.get(i);
+            if ((notSystemApp(p))) {
+                String appName = p.applicationInfo.loadLabel(pm).toString();
+                Drawable icon = p.applicationInfo.loadIcon(pm);
+                String packages = p.applicationInfo.packageName;
+                apps.add(new app_data(appName, icon, packages));
+            }
+        }
+        return apps;
+    }
+
+    private boolean notSystemApp(PackageInfo pkgInfo) {
+        return (pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0;
+    }
 }
+
+
+
+
 
--- app/src/main/java/com/example/batterycheck/MyAdapter.java
+++ app/src/main/java/com/example/batterycheck/MyAdapter.java
@@ -1,6 +1,10 @@
 package com.example.batterycheck;
 
+import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,14 +23,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyOwnHolder> {
-    private ArrayList<app_data> apps_list;
+    public ArrayList<MainActivity.app_data> apps_list;
     Context ctx;
-    String data1[],data2[];
+    String data1[], data2[];
     int img[];
 
-    public MyAdapter(Context ct){
-        ctx=ct;
-        apps_list=get_apps_list();
+    public MyAdapter(Context ct,ArrayList<MainActivity.app_data> apps_list) {
+        ctx = ct;
+        this.apps_list = apps_list;
     }
 
     /*Below method is the place where we will need the row data.
@@ -38,8 +43,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyOwnHolder> {
         /*
         Inflater classes are capable of inflating your xml file to your java file
          */
-        LayoutInflater myInflator= LayoutInflater.from(ctx); //The way to get the object of layout inflater
-        View myOwnView= myInflator.inflate(R.layout.app_list,parent,false);
+        LayoutInflater myInflator = LayoutInflater.from(ctx); //The way to get the object of layout inflater
+        View myOwnView = myInflator.inflate(R.layout.app_list, parent, false);
         return new MyOwnHolder(myOwnView);
     }
 
@@ -48,16 +53,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyOwnHolder> {
     //we already have prepared (dummy data)
     public void onBindViewHolder(@NonNull MyOwnHolder holder, int position) {
 
-        if(apps_list.get(position)!=null){
-            holder.app_name.setText(apps_list.get(position).name);
-            holder.app_package_name.setText(apps_list.get(position).packages);
-            holder.extra_text.setText(apps_list.get(position).name);
+        if (apps_list.get(position) != null) {
+            holder.app_name.setText(apps_list.get(position).getName());
+            holder.app_package_name.setText(apps_list.get(position).getPackages());
+            holder.extra_text.setText(apps_list.get(position).getName());
             holder.app_icon.setImageDrawable(apps_list.get(position).icon);
-//            if(data1[position].equals("Rabbit")){
-//                holder.itemView.setBackgroundColor(0xffffff00);
-//            }
         }
-
     }
 
     @Override
@@ -74,67 +75,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyOwnHolder> {
         After the onCreateViewHolder() the control reaches the MyOwnHolder constructor as a new object
         of the MyOwnHolder is returned by that function
          */
-    public class MyOwnHolder extends RecyclerView.ViewHolder{
-        TextView app_name,app_package_name, extra_text;
+    public class MyOwnHolder extends RecyclerView.ViewHolder {
+        TextView app_name, app_package_name, extra_text;
         ImageView app_icon;
+
         public MyOwnHolder(@NonNull View itemView) {
             super(itemView);
-            app_name= (TextView)itemView.findViewById(R.id.app_name);
-            app_package_name= (TextView)itemView.findViewById(R.id.app_package_name);
-            extra_text= (TextView)itemView.findViewById(R.id.extra_info);
-            app_icon= (ImageView)itemView.findViewById(R.id.app_icon);
+            app_name = (TextView) itemView.findViewById(R.id.app_name);
+            app_package_name = (TextView) itemView.findViewById(R.id.app_package_name);
+            extra_text = (TextView) itemView.findViewById(R.id.extra_info);
+            app_icon = (ImageView) itemView.findViewById(R.id.app_icon);
         }
+
     }
-
-
-    public class app_data {
-        private String name;
-        Drawable icon;
-        private String packages;
-
-        //        private String a; //declare more parameters
-//        private int d; //declare more parameters
-        public app_data(String name, Drawable icon, String packages) {   //}, String a, int d) {
-            this.name = name;
-            this.icon = icon;
-            this.packages = packages;
-//            this.a = a;
-//            this.d = d;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Drawable getIcon() {
-            return icon;
-        }
-
-        public String getPackages() {
-            return packages;
-        }
-    }
-
-    private ArrayList<app_data> get_apps_list() {
-        PackageManager pm = ctx.getPackageManager();
-        ArrayList<app_data> apps = new ArrayList<app_data>();
-        List<PackageInfo> packs = pm.getInstalledPackages(0);
-        //List<PackageInfo> packs = getPackageManager().getInstalledPackages(PackageManager.GET_PERMISSIONS);
-        for (int i = 0; i < packs.size(); i++) {
-            PackageInfo p = packs.get(i);
-            if ((notSystemApp(p))) {
-                String appName = p.applicationInfo.loadLabel(pm).toString();
-                Drawable icon = p.applicationInfo.loadIcon(pm);
-                String packages = p.applicationInfo.packageName;
-                apps.add(new app_data(appName, icon, packages));
-            }
-        }
-        return apps;
-    }
-
-    private boolean notSystemApp(PackageInfo pkgInfo) {
-        return (pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0;
-    }
-
 }
-
